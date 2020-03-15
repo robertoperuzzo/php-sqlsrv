7bf46ee10b24a441f1b04050cc7d7d9d4d1e2af6
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2020-03-15 09:10:17 +0100

    Fixing Checking PHP modules error // Re-added newline at eof on save.

--- 7/tests/tests.sh
+++ 7/tests/tests.sh
@@ -19,7 +19,7 @@ fi
 
 if ! cmp -s ~/php_modules.tmp ~/expected_modules; then
     echo "Error. PHP modules are not identical."
-    diff ~/php_modules.tmp ~/expected_modules
+    diff -c ~/php_modules.tmp ~/expected_modules
     exit 1
 fi
 
